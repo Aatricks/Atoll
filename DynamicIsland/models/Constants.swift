c677c815f3629a152cffc023c31ba653d239f6b3
Aatricks <melis.emilio1@gmail.com>
2026-08-14 17:55:32 -0400

    Add prominentOpposingColors to NSImage+Extensions, CiderController to MediaControllers, and cider case to MediaControllerType

--- DynamicIsland/models/Constants.swift
+++ DynamicIsland/models/Constants.swift
@@ -410,6 +410,7 @@ enum MediaControllerType: String, CaseIterable, Identifiable, Defaults.Serializa
     case spotify = "Spotify"
     case youtubeMusic = "Youtube Music"
     case amazonMusic = "Amazon Music"
+    case cider = "Cider"
     
     var id: String { self.rawValue }
     
@@ -420,6 +421,7 @@ enum MediaControllerType: String, CaseIterable, Identifiable, Defaults.Serializa
         case .spotify: return String(localized: "Spotify")
         case .youtubeMusic: return String(localized: "Youtube Music")
         case .amazonMusic: return String(localized: "Amazon Music")
+        case .cider: return String(localized: "Cider")
         }
     }
 }
